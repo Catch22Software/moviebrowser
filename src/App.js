6155f831174abd5dd359024d9c26c59758ffcc54
zeldabedabomb <88691158+zeldabedabomb@users.noreply.github.com>
2022-02-03 13:26:43 -0500

    Save before making possible breaking changes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import AboutView from "./components/AboutView";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div>
         <Navbar/>
-        <Home/>
+            <Routes>
+                <Route path="/welcome"  element={<Home/>}/>
+                <Route path="/about" element={<AboutView/>}/>
+            </Routes>
     </div>
   );
 }
